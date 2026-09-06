621a2292758f9252101595130f2a5ecf6948dd8f
subodhcoc222-dev <subodhcoc222@gmail.com>
2026-09-06 12:23:30 +0530

    Create DeskConfig.kt

---
+++ app/src/main/java/com/desk/companion2/DeskConfig.kt
@@ -0,0 +1,10 @@
+package com.desk.companion2
+
+object DeskConfig {
+    /**
+     * आपके Desk Sentry कैमरा फोन की स्क्रीन पर ऊपर जो 6-digit Device ID दिखती है,
+     * उसे यहाँ दर्ज करें (उदा. "748291"):
+     */
+    const val TARGET_DEVICE_ID = "100001" 
+    const val FIREBASE_ROOT_NODE = "desk_sentry"
+}
