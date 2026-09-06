37b6726a60a5718478f91eed4e15c879e47e24e8
subodhcoc222-dev <subodhcoc222@gmail.com>
2026-09-06 13:20:55 +0530

    Update DeskConfig.kt

--- app/src/main/java/com/desk/companion2/DeskConfig.kt
+++ app/src/main/java/com/desk/companion2/DeskConfig.kt
@@ -1,7 +1,7 @@
 package com.desk.companion2
 
 object DeskConfig {
-    // 100% Hardcoded Camera Cloud ID (No user editing allowed)
+    // 100% Hardcoded Target ID - No manual pairing required
     const val TARGET_DEVICE_ID = "349806"
     const val FIREBASE_ROOT_NODE = "desk_sentry"
 }
